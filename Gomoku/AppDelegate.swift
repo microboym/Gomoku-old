6d95e232a3ae077a8e4db352899c2278aa680aed
Tony Mai <microboym@outlook.com>
2020-01-28 19:23:19 +0800

    Initial Commit

---
+++ Gomoku/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gomoku
+//
+//  Created by Tony on 1/28/20.
+//  Copyright © 2020 Tony. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Gomoku/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Gomoku
+//
+//  Created by Tony on 1/28/20.
+//  Copyright © 2020 Tony. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
